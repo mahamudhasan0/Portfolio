3242a9cf88bc33bfddbf09d2009c7551f79174fa
Mahamud Hasan <mahmudhasan960458@gmail.com>
2021-05-02 15:52:32 +0600

    title add

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -13,7 +13,7 @@ const Contact = () => {
         </div>
         <div className="Contact">
             <div className="map-sect">
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d468468.405440254!2d89.16255896836942!3d23.463731689704044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fe55c9b76dc93f%3A0xcab06b14c22acb00!2sMagura%20District!5e0!3m2!1sen!2sbd!4v1619908628359!5m2!1sen!2sbd" width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy"></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d468468.405440254!2d89.16255896836942!3d23.463731689704044!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fe55c9b76dc93f%3A0xcab06b14c22acb00!2sMagura%20District!5e0!3m2!1sen!2sbd!4v1619908628359!5m2!1sen!2sbd" width="600" height="450" style={{border:0}} allowFullScreen=""  title="This is a unique title" loading="lazy"></iframe>
             </div>
             <div className="contact-section">
                 <Item icon={phone} text1={'01316188019'} text2={'01786828485'} title={'Phone'}/>
--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -3,8 +3,8 @@ import { Document, Page } from 'react-pdf';
 import samplePDF from '../../Images/mahamudhasan mithoon(2).pdf';
 
 export default function Resume() {
-        const [numPages, setNumPages] = useState(null);
-        const [pageNumber, setPageNumber] = useState(1);
+        const [setNumPages] = useState(null);
+        const [pageNumber] = useState(1);
       
         function onDocumentLoadSuccess({ numPages }) {
           setNumPages(numPages);
